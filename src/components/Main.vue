67b969ebd6e199d03a0ca38380d06b0c9adff438
roma_marshall <roma_marshall@outlook.com>
2024-10-18 16:45:44 +0200

    added data table

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,11 +15,85 @@
       ></div>
     </div>
     <div
-        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
+        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 min-h-96 mb-4"
     >
 <!--      <div v-for="item in coinData">-->
 <!--        {{ item.symbol }}-->
 <!--      </div>-->
+
+
+
+
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 whitespace-nowrap">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              #
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" class="px-6 py-3">
+              24h %
+            </th>
+            <th scope="col" class="px-6 py-3">
+              ATH %
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Volume(24h)
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Market Cap
+            </th>
+            <th scope="col" class="px-6 py-3">
+              FDV
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Circulating Supply
+            </th>
+          </tr>
+          </thead>
+          <tbody class="text-black">
+          <tr v-for="item in coinData" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {{ item.market_cap_rank }}
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <div class="whitespace-normal">
+                <p class="font-semibold">{{ item.name }}</p>
+                <p class="text-slate-500 font-normal uppercase">{{ item.symbol }}</p>
+              </div>
+            </th>
+            <td class="px-6 py-4">
+              ${{ item.current_price }}
+            </td>
+            <td class="px-6 py-4">
+              {{ item.price_change_percentage_24h.toFixed(2) }} %
+            </td>
+            <td class="px-6 py-4">
+              {{ item.ath_change_percentage.toFixed(2) }} %
+            </td>
+            <td class="px-6 py-4">
+              123
+            </td>
+            <td class="px-6 py-4">
+              ${{ item.market_cap }}
+            </td>
+            <td class="px-6 py-4">
+              ${{ item.fully_diluted_valuation }}
+            </td>
+            <td class="px-6 py-4">
+              {{ item.circulating_supply }}
+              <span class="uppercase">{{ item.symbol }}</span>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
 
 
 
