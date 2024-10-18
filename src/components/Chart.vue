f48a113ef5ae81b76c8fbad1f31a0a0c04a625f8
roma_marshall <roma_marshall@outlook.com>
2024-10-18 20:51:29 +0200

    added dominance charts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -6,13 +6,13 @@
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">Bitcoin Dominance</p>
       </div>
       <div class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-        {{ globalData?.data.market_cap_percentage.btc.toFixed(2) }}
+        ${{ formatPrice(totalMC) }}
         <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
         </svg>
       </div>
     </div>
-    <Apexchart type="area" height="100%" :options="chartOptions" :series="chartOptions.series" />
+    <Apexchart type="donut" height="100%" :options="chartOptions" :series="chartOptions.series" />
   </div>
 </template>
 
@@ -23,81 +23,102 @@ import { usePiniaStore } from '../store/pinia.js'
 
 const Apexchart = VueApexCharts
 const store = usePiniaStore()
-const globalData = computed(() => store.globalData)
 
-// const hourlyTimestamps = computed(() => store.hourlyTimestamps)
-// const btcD = computed(() => store.btcD)
+const btc = computed(() => store.globalData?.data.market_cap_percentage.btc.toFixed(2))
+const eth = computed(() => store.globalData?.data.market_cap_percentage.eth.toFixed(2))
+const bnb = computed(() => store.globalData?.data.market_cap_percentage.bnb.toFixed(2))
+const sol = computed(() => store.globalData?.data.market_cap_percentage.sol.toFixed(2))
+const usdt = computed(() => store.globalData?.data.market_cap_percentage.usdt.toFixed(2))
+const totalMC = computed(() => store.globalData?.data.total_market_cap.usd)
 
 const chartOptions = ref({
+  series: [Number(btc.value), Number(eth.value), Number(bnb.value), Number(sol.value), Number(usdt.value)],
+  colors: ["#1C64F2", "#16BDCA", "#FDBA8C", "#E74694", "#8A4FFF"],
   chart: {
-    height: "100%",
-    maxWidth: "100%",
-    type: "area",
-    fontFamily: "Inter, sans-serif",
-    dropShadow: {
-      enabled: false,
-    },
-    toolbar: {
-      show: false,
+    height: 320,
+    width: "100%",
+    type: "donut",
+  },
+  stroke: {
+    colors: ["transparent"],
+    lineCap: "",
+  },
+  plotOptions: {
+    pie: {
+      donut: {
+        labels: {
+          show: true,
+          name: {
+            show: true,
+            fontFamily: "Inter, sans-serif",
+            offsetY: 20,
+          },
+          total: {
+            showAlways: true,
+            show: true,
+            label: "Unique visitors",
+            fontFamily: "Inter, sans-serif",
+            formatter: function (w) {
+              const sum = w.globals.seriesTotals.reduce((a, b) => {
+                return a + b;
+              }, 0);
+              return 'test'
+            },
+          },
+          value: {
+            show: true,
+            fontFamily: "Inter, sans-serif",
+            offsetY: -20,
+            formatter: function (value) {
+              return value + "%";
+            },
+          },
+        },
+        size: "80%",
+      },
     },
   },
-  tooltip: {
-    enabled: true,
-    x: {
-      show: false,
+  grid: {
+    padding: {
+      top: -2,
     },
   },
-  fill: {
-    type: "gradient",
-    gradient: {
-      opacityFrom: 0.55,
-      opacityTo: 0,
-      shade: "#1C64F2",
-      gradientToColors: ["#1C64F2"],
-    },
-  },
+  labels: ["BTC", "ETH", "BNB", "SOL", "USDT"],
   dataLabels: {
     enabled: false,
   },
-  stroke: {
-    width: 6,
+  legend: {
+    position: "bottom",
+    fontFamily: "Inter, sans-serif",
   },
-  grid: {
-    show: false,
-    strokeDashArray: 4,
-    padding: {
-      left: 2,
-      right: 2,
-      top: 0
-    },
-  },
-  series: [
-    {
-      name: "BTC.D",
-      data: [1,2,3,5,6],
-      color: "#1A56DB",
-    },
-  ],
-  xaxis: {
-    categories: [1,2,3,4,5,6],
+  yaxis: {
     labels: {
+      formatter: function (value) {
+        return value + "%";
+      },
+    },
+  },
+  xaxis: {
+    labels: {
+      formatter: function (value) {
+        return value + "%";
+      },
+    },
+    axisTicks: {
       show: false,
     },
     axisBorder: {
       show: false,
     },
-    axisTicks: {
-      show: false,
-    },
   },
-  yaxis: {
-    show: false,
-  },
-})
+});
+
+const formatPrice = (value, toFixed) => {
+  let val = (value/1).toFixed(toFixed).replace(',', '.')
+  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
 
 onMounted(() => {
   store.fetchGlobalData()
-  // store.fetchBTCD()
-  // store.generateHourlyTimestamps()
 })
 </script>
--- src/store/pinia.js
+++ src/store/pinia.js
@@ -7,10 +7,6 @@ export const usePiniaStore = defineStore('data', {
     globalData: null,  // API data storage
     loading: false,  // to monitor the status of the download
     error: null,     // to track errors
-    hourlyTimestamps: [],
-    firstTimestamp: null,
-    lastTimestamp: null,
-    btcD: null
   }),
 
   actions: {
@@ -75,77 +71,6 @@ export const usePiniaStore = defineStore('data', {
 
         this.globalData = data   // saving the data to the Pinia store
         return this.globalData
-
-      } catch (error) {
-        this.error = error
-        throw error  // throw an error for processing at the component level
-      } finally {
-        this.loading = false
-      }
-    },
-
-    async generateHourlyTimestamps() {
-      const timestamps = []
-      const now = new Date()
-
-      // set the date to the beginning of the current day (midnight)
-      const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
-
-      // function for formatting timestamp in the format dd.mm.YY, HH:MM
-      const formatTimestamp = (timestamp) => {
-        const date = new Date(timestamp * 1000)
-        const day = String(date.getDate()).padStart(2, '0')
-        const month = String(date.getMonth() + 1).padStart(2, '0')
-        const year = String(date.getFullYear()).slice(-2)
-        const hours = String(date.getHours()).padStart(2, '0')
-        const minutes = String(date.getMinutes()).padStart(2, '0')
-
-        return `${day}.${month}.${year}, ${hours}:${minutes}`
-      }
-
-      // generating timestamps for each hour
-      for (let i = 0; i < 24; i++) {
-        const timestamp = Math.floor(new Date(startOfDay.getTime() + i * 60 * 60 * 1000).getTime() / 1000)
-        // timestamps.push(formatTimestamp(timestamp))
-        timestamps.push(timestamp)
-      }
-
-      this.firstTimestamp = timestamps[0]
-      this.lastTimestamp = timestamps[timestamps.length - 1]
-
-      this.hourlyTimestamps = timestamps   // saving the data to the Pinia store
-      return this.hourlyTimestamps
-    },
-
-    async fetchBTCD() {
-      // check if the data already exists in the storage, then return it
-      if (this.btcD) {
-        return this.btcD
-      }
-
-      this.loading = true
-      this.error = null
-
-      const from = 1729202400
-      const to = 1729285200
-
-      try {
-        const response = await fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${from}&to=${to}`, {
-          headers: {
-            'x_cg_demo_api_key': import.meta.env.VITE_API_KEY,
-          },
-        })
-
-
-        // check if the request is successful
-        if (!response.ok) {
-          throw new Error('Failed to fetch data')
-        }
-
-        const data = await response.json()
-
-        this.btcD = data   // saving the data to the Pinia store
-        return this.btcD
 
       } catch (error) {
         this.error = error
