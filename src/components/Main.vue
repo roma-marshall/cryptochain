e1e7bd505a55e060174f931217c4f3f297861447
roma_marshall <roma_marshall@outlook.com>
2024-10-19 12:06:42 +0200

    added order

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="p-4 md:ml-64 h-auto pt-28 sm:pt-20">
-    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mb-4">
+  <main class="flex flex-col p-4 md:ml-64 h-auto pt-28 sm:pt-20">
+    <div class="order-1 lg:order-0 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mb-4">
       <div
           class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 min-h-32 md:min-h-64"
       >
@@ -16,11 +16,11 @@
       ></div>
     </div>
     <div
-        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 min-h-96 mb-4"
+        class="order-0 lg:order-1 border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 min-h-96 mb-4"
     >
       <CoinList />
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+    <div class="order-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
       <div
           class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"
       ></div>
@@ -34,7 +34,7 @@
           class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
       ></div>
     </div>
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+    <div class="order-4 grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
       <div
           class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
       ></div>
@@ -49,9 +49,9 @@
       ></div>
     </div>
     <div
-        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
+        class="order-5 border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
     ></div>
-    <div class="grid grid-cols-2 gap-4">
+    <div class="order-6 grid grid-cols-2 gap-4">
       <div
           class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
       ></div>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -331,7 +331,7 @@
       </ul>
     </div>
     <div
-        class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20"
+        class="hidden absolute bottom-10 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20"
     >
       <a
           href="#"
